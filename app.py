9800e0af53a0b7d1d16c10aa1c8547b1931510a1
ignacyr <ignacy.rogatty@gmail.com>
2022-06-23 01:30:10 +0200

    added some charts: pie, inflation, average

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from datetime import date, datetime
+
 from dash import dcc, html, Dash
 from dash.dependencies import Input, Output
 import pandas as pd
@@ -11,43 +14,74 @@ server = app.server
 password_reporting_user = "plzanalyzeme69"
 conn = sqlalchemy.create_engine(
     f"postgresql://reporting_user:{password_reporting_user}@it-offers.c9umk0ew1r8h.us-east-1.rds.amazonaws.com:5432/dwh")
-df = pd.read_sql_query(
-    sql="SELECT title, skills, category, level, company, date, min_salary, max_salary FROM offers",
-    con=conn)
 
 # Layout
 app.layout = html.Div([
-    html.H1("IT offers dashboards", style={'text-align': 'center'}),
+    html.H1("IT offers dashboards at specific date", style={'text-align': 'center'}),
 
     dcc.Dropdown(id="select-day",
                  options=[
                      {"label": "20-06-2022", "value": 20220620},
-                     {"label": "21-06-2022", "value": 20220621}],
+                     {"label": "21-06-2022", "value": 20220621},
+                     {"label": "22-06-2022", "value": 20220622}],
                  multi=False,
-                 value=20220620,
+                 value=20220622,
                  style={"width": "40%"}),
 
-    html.Br(),
+    dcc.Graph(id="it-offers-salary", figure={}),
+    dcc.Graph(id="skills-pop", figure={}),
 
-    dcc.Graph(id="it-offers-min-salary", figure={}),
-    dcc.Graph(id="it-offers-max-salary", figure={})
+
+    html.H2("IT offers inflation", style={'text-align': 'center'}),
+
+    dcc.Graph(id="average-salary", figure={}),
 ])
 
 
+# Update graph
 @app.callback(
-    [Output(component_id="it-offers-min-salary", component_property="figure"),
-     Output(component_id="it-offers-max-salary", component_property="figure")],
+    [Output(component_id="it-offers-salary", component_property="figure"),
+     Output(component_id="skills-pop", component_property="figure"),
+     Output(component_id="average-salary", component_property="figure")],
     [Input(component_id="select-day", component_property="value")]
 )
 def update_graph(option_selected):
+    df = pd.read_sql_query(
+        sql="SELECT title, skills, category, level, company, date, min_salary, max_salary FROM offers",
+        con=conn)
+
+    # additional column of mean salary
+    df["salary"] = df[["min_salary", "max_salary"]].mean(axis=1)
+
     dff = df.copy()
     dff = dff[dff["date"] == option_selected]
 
     # plotly express
-    min_salary_fig = px.histogram(dff, x="min_salary", nbins=100)
-    max_salary_fig = px.histogram(dff, x="max_salary", nbins=100)
+    salary_fig = px.histogram(dff, x="salary", nbins=100)
+    salary_fig.update_layout(bargap=0.1, xaxis_title_text='Salary', yaxis_title_text='Count',
+                             title_text='Offered salary for IT specialists')
+    
+    # skills extraction from 'skills' column
+    skill_sets = dff.skills.apply(lambda x: x[1:-1].split(','))
+    skills = [skill.replace("'", '').replace(' ', '') for skill_set in skill_sets for skill in skill_set]
+    skills = [skill for skill in skills if skill]
 
-    return min_salary_fig, max_salary_fig
+    # count skills
+    skills_count = dict(Counter(skills))
+
+    # pie chart
+    skills_pop_fig = px.pie(values=skills_count.values(), names=skills_count.keys(),
+                            title='Popularity of different skills',
+                            width=1600, height=1000)
+
+    # avg salary
+    avg_salary_df = df.groupby("date")[["min_salary", "salary", "max_salary"]].mean()
+    avg_salary_df["date"] = pd.to_datetime(avg_salary_df.index, format='%Y%m%d')
+    avg_salary_fig = px.scatter(avg_salary_df, x="date", y="salary", trendline="ols")
+    avg_salary_fig.update_layout(xaxis_title_text="Day", yaxis_title_text="Salary")
+    avg_salary_fig.update_xaxes(dtick="D1")
+
+    return salary_fig, skills_pop_fig, avg_salary_fig
 
 
 if __name__ == "__main__":
